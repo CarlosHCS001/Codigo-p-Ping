ec9fa23a0a74c377e17444708f9308c2259ac08d
Carlao henri C S <79725152+CarlosHCS001@users.noreply.github.com>
2022-01-25 21:25:01 -0300

    Update pingsimples.py

--- pingsimples.py
+++ pingsimples.py
@@ -1,11 +1,9 @@
-import os ##Importa o modulo ou biblioteca os (integra os programas e
-#recursos do S.O
+import os 
 
-
-print("#" * 60) ##Imprimindo 60 vezes
+print("#" * 60) 
 
 ip_ou_host = input("Digite o ip ou host a ser verificacao: ")
-#criamos uma variavel que vai receber do usuario um ip
-print("-" * 60) ##Imprimindo - 60 vezes
-os.system('ping -n 6 {}'.format(ip_ou_host)) ##chamando system da biblioteca os - comando ping
-print("-" * 60) ##imprimindo - 60 vezes
+
+print("-" * 60)
+os.system('ping -n 6 {}'.format(ip_ou_host)) 
+print("-" * 60) 
